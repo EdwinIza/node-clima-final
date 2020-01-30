45eac5403b5ff70a9299778a76510a3e900a3f26
Edwin <eizac@est.ups.edu.ec>
2020-01-30 01:37:54 -0500

    first commit

---
+++ controlador/clima.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+//OBTENCIÓN DEL CLIMA
+const getClima = async(lat, lon) => {
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ac7eaef281e24b3e15ae43d9c6ee567d&units=metric`);
+    return resp.data.main.temp;
+}
+
+module.exports = {
+    getClima
+}
